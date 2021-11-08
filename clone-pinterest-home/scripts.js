2ca26ae5e5e5b2d11be31205dfd3c30333e96306
viniciusm7 <melgaco97@outlook.com>
2021-11-08 13:31:04 -0300

    Add algumas imagens

--- clone-pinterest-home/scripts.js
+++ clone-pinterest-home/scripts.js
@@ -1,15 +1,55 @@
 const grids = document.querySelectorAll('.grid');
 const headings = document.querySelectorAll('.heading .wrapper .text');
 
-function enterScreen(index) {}
-function exitScreen(index, exiteDelay) {}
+function enterScreen(index) {
+    const grid = grids[index]
+    const heading = headings[index]
+    const gridColumns = grid.querySelectorAll('.column')
+  
+    grid.classList.add('active')
+  
+    gridColumns.forEach(element => {
+      element.classList.remove('animate-before', 'animate-after')
+    })
+  
+    heading.classList.remove('animate-before', 'animate-after')
+  }
+  function exitScreen(index, exitDelay) {
+    const grid = grids[index]
+    const heading = headings[index]
+    const gridColumns = grid.querySelectorAll('.column')
+  
+    gridColumns.forEach(element => {
+      element.classList.add('animate-after')
+    })
+  
+    heading.classList.add('animate-after')
+  
+    setTimeout(() => {
+      grid.classList.remove('active')
+    }, exitDelay)
+  }
 
-function SetupAnimationCycle({initalScreenIndex, timePerScreen, exitDelay}){
+function SetupAnimationCycle({ timePerScreen,exitDelay }){
+    const cycleTime = timePerScreen + exitDelay; 
+    let nextIndex = 0
+    
+    function nextCycle(){
+        const currentIndex = nextIndex
+        
+        enterScreen(currentIndex)
+
+        setTimeout(() => exitScreen(currentIndex, exitDelay), timePerScreen);
+
+        // se o proxomo index for maior.. 
+        nextIndex = nextIndex >= grids.length -1 ? 0 : nextIndex + 1 
+    }
+
+    setInterval(nextCycle, cycleTime);
 
 }
 
 SetupAnimationCycle({
-    initalScreenIndex:0,
     timePerScreen:2000,
     exitDelay: 200 * 7
 })
