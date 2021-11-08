201ab824ccb59c04c7b4e8325664d003fd479976
viniciusm7 <melgaco97@outlook.com>
2021-11-08 10:26:27 -0300

    startd animation e scripts

---
+++ clone-pinterest-home/scripts.js
@@ -0,0 +1,15 @@
+const grids = document.querySelectorAll('.grid');
+const headings = document.querySelectorAll('.heading .wrapper .text');
+
+function enterScreen(index) {}
+function exitScreen(index, exiteDelay) {}
+
+function SetupAnimationCycle({initalScreenIndex, timePerScreen, exitDelay}){
+
+}
+
+SetupAnimationCycle({
+    initalScreenIndex:0,
+    timePerScreen:2000,
+    exitDelay: 200 * 7
+})
